ea9e6e9114d4e83ce21c018e073de8f3e99de419
manuelkwizera <kwizeraemmanuel11@gmail.com>
2023-12-13 08:35:36 +0530

    create project layouts

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php include 'layouts/head.php'; ?>
+    <body>
+        <?php include 'layouts/navbar.php'; ?>
+        <div class="full-title">
+            <div class="container">
+                <h1 class="mt-4 mb-3"></h1>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="about-main">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <h2>Our Mission</h2>
+                        <p style="text-align: justify;">At Geneius Environmental, our mission is to pioneer sustainable solutions at the intersection of industry, ambient air quality, and construction, safeguarding the very air and environment upon which our lives depend. We specialize in partnering with large industries to optimize chimney/stack emissions, ensuring they meet stringent environmental standards. Our expertise helps these businesses reduce their carbon footprint and foster responsible industrial practices. We are committed to the relentless pursuit of clean, breathable air for all. Through state-of-the-art ambient air quality monitoring, we provide invaluable insights to communities and businesses, enabling them to make informed decisions that protect health and environment. In the realm of construction, we champion eco-friendly building practices that harmonize with nature. Our guidance ensures that construction activities minimize their impact on the environment, fostering sustainable urban development. Geneius Environmental embodies the ethos of harmony between industry, air quality, and the environment. We are dedicated to a future where businesses thrive in tandem with the well-being of the air we breathe and the world we call home.</p>                       
+                    </div>
+
+                    <div class="col-lg-6">
+                        <img class="img-fluid rounded" src="images/new/about-img.jpg" alt="" />
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-6">
+                        <h2>Our Vision</h2>
+                        <p style="text-align: justify;">Creating a real-time emission compliance system based on local and regional standards, with scientific evidence for emission quantification and air degradation. Offering tools, reports for EIA non-compliance, and maintaining an air pollution data archive.</p>
+                    </div>
+                    <div class="col-lg-6">
+                        <h2>Our Aproach</h2>
+                        <p style="text-align: justify;">We collaborate with industries, construction firms, concert organizers, and corporations to provide support and insights on the potential sources of pollution and provide mitigation strategies</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="services-bar mb-4">
+                <h1 class="my-4">Our Services </h1>
+                <div class="row">
+                    <div class="col-lg-6 mb-6">
+                        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                            <div class="carousel-inner" style="height: 350px;">
+                                <div class="carousel-item active">
+                                    <img class="d-block w-100" src="images/new/services/service-12.jpeg" alt="First slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="images/new/services/service-2.jpeg" alt="Second slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="images/new/services/service-8.jpeg" alt="Third slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="images/new/services/service-9.jpeg" alt="fourth slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="images/new/services/list/service-8.jpeg" alt="fifth slide">
+                                </div>
+                            </div>
+
+                            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+
+                            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 mb-6">
+                        <ul style="text-transform: capitalize; font-size: medium;">
+                            <li class="test"><a href="services/ambient-and-indoor-air-quality-measurements.html" style="color: inherit;">Ambient and indoor air quality measurements</a></li>
+                            <li class="test"><a href="services/chimney-and-stack-emissions-monitoring.html" style="color: inherit;">Chimney and Stack Emissions Monitoring</a></li>
+                            <li class="test"><a href="services/noise-pollution-monitoring-and-advisory.html" style="color: inherit;">Noise Pollution  Monitoring  And Advisory</a></li>
+                            <li class="test"><a href="services/environmental-health-and-safety-trainings-and-advisory.html" style="color: inherit;">Environmental, health and Safety Trainings and Advisory</a></li>
+                            <li class="test"><a href="services/vibration-monitoring-and-advisory-for-blasting-activities.html" style="color: inherit;">Vibration Monitoring and Advisory for Blasting Activities</a></li>
+                            <li class="test"><a href="services/wastewater-management-and-treatment.html" style="color: inherit;">Wastewater Management and Treatment</a></li>
+                            <li class="test"><a href="services/environmental_and_social_impact_assessment.html" style="color: inherit;">Environmental and social Impact assessment.</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="map-bar mb-4">
+                    <h1 class="my-4">Our Global Experience</h1>
+                    <div class="row">
+                        <div class="col-lg-12 mb-6">
+                            <iframe width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;ll=-1.940278,29.873888&amp;z=8&amp;output=embed"></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="customers-box">
+                <h2>Our Customers</h2>
+                <div class="row">
+                    <div class="col-lg-3 col-sm-4 mb-4">
+                        <img class="img-fluid" src="images/new/customers/logo-1.jpg" alt="" />
+                    </div>
+                    <div class="col-lg-2 col-sm-4 mb-4">
+                        <img class="img-fluid" src="images/new/customers/logo-2.jpeg" alt="" />
+                    </div>
+                    <div class="col-lg-2 col-sm-4 mb-4">
+                        <img class="img-fluid" src="images/new/customers/logo-4.jpg" alt="" />
+                    </div>
+                    <div class="col-lg-2 col-sm-4 mb-4">
+                        <img class="img-fluid" src="images/new/customers/logo-5.jpg" alt="" />
+                    </div>
+                    <div class="col-lg-3 col-sm-4 mb-4">
+                        <img class="img-fluid" src="images/new/customers/logo-6.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <?php include 'layouts/footer.php'; ?>
+        <?php include 'layouts/scripts.php'; ?>
+    </body>
+</html>
---
+++ layouts/navbar.php
@@ -0,0 +1,52 @@
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-light top-nav fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="index.html">
+            <img src="images/new/logo.png" alt="logo" />
+        </a>
+
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="fas fa-bars"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" href="index.html">Home</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="about.html">About</a>
+                </li>
+
+                <li class="nav-item dropdown service-dropdown">
+                    <a class="nav-link" href="services.html" id="service-dropdown">Services <i class="fas fa-caret-down"></i></a>
+                    <div class="dropdown-menu dropdown-menu-right service-dropdown-menu" aria-labelledby="navbarDropdownBlog">
+                        <a class="dropdown-item" href="services/chimney-and-stack-emissions-monitoring.html" style="text-transform: uppercase;">Chimney And Stack Emissions Monitoring</a>
+                        <a class="dropdown-item" href="services/ambient-and-indoor-air-quality-measurements.html" style="text-transform: uppercase;">Ambient And Indoor Air Quality Measurements</a>
+                        <a class="dropdown-item" href="services/noise-pollution-monitoring-and-advisory.html" style="text-transform: uppercase;">Noise Pollution Monitoring And Advisory</a>
+                        <a class="dropdown-item" href="services/environmental-health-and-safety-trainings-and-advisory.html" style="text-transform: uppercase;">Environmental, Health And Safety Trainings And Advisory</a>
+                        <a class="dropdown-item" href="services/vibration-monitoring-and-advisory-for-blasting-activities.html" style="text-transform: uppercase;">VVibration Monitoring And Advisory For Blasting Activities </a>
+                        <a class="dropdown-item" href="services/wastewater-management-and-treatment.html" style="text-transform: uppercase;">Wastewater Management And Treatment</a>
+                        <a class="dropdown-item" href="services/environmental_and_social_impact_assessment.html" style="text-transform: uppercase;"> Environmental and social Impact assessment</a>
+                    </div>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link"  href="projects.html">Projects</a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">RESOURCES</a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+                        <a class="dropdown-item" href="faq.html">FAQ</a>
+                        <a class="dropdown-item" href="testimonials.html">TESTIMONIALS</a>
+                    </div>
+                </li>
+                
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.html">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
