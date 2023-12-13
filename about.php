b66d70056c5e316306f76c32be0c1859c1b1eeb4
manuelkwizera <kwizeraemmanuel11@gmail.com>
2023-12-13 08:46:37 +0530

    creating about page

---
+++ about.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php include('layouts/head.php') ?>
+    <body>
+        <?php include('layouts/navbar.php') ?>       
+        <div class="container">
+            <div class="breadcrumb-main">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item">
+                        <a href="index.html">Home</a>
+                    </li>
+                    <li class="breadcrumb-item active">About</li>
+                </ol>
+            </div>
+
+            <div class="about-main">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <img class="img-fluid rounded mb-4" src="images/new/about-img.jpg" alt="" />
+                    </div>
+                    <div class="col-lg-6">
+                        <h2>About Us</h2>
+                        <p style="text-align: justify;">Genius Environmental Ltd., formerly Estonteco Brila Ltd., is a pioneering private company that has firmly established itself within the environmental sector in Rwanda. With a strong commitment to sustainable practices and environmental stewardship, we have earned recognition as a Lead Expert Firm by the Rwanda Association of Environmental Practitioners (RAPEP), holding the prestigious registration number RAPEP/EA/229.</p>
+                        <p style="text-align: justify;">At Genius Environmental Ltd., innovation is at the core of our ethos. We constantly seek new and groundbreaking approaches to tackle environmental challenges. Our projects are not just about compliance; they are about driving meaningful change. We take pride in our ability to deliver cost-effective solutions that not only meet regulatory requirements but also enhance the overall quality of life for communities and ecosystems. Our work leaves a lasting positive impact, creating a legacy of environmental sustainability for the regions we serve.</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="team-members-box">
+                <h2 style="text-align: center;">Our Team</h2>
+                <div class="row mt-5">
+                    <div class="col-lg-6">
+                        <div class="media">
+                            <img class="d-flex mr-3 rounded-circle" src="images/new/team/team-8.jpg" alt="">
+                            <div class="media-body">
+                              <h5 class="mt-0">Eng. Theobald HABINEZA</h5>
+                              <p style="font-size: small;">Chief Operations Officer</p>
+                              <p style="text-align: justify;">Ph.D. Student in atmospheric science with a focus on air pollution at Center of Atmospheric Particle Studies (CAPS) at Carnegie Mellon University Pittsburgh, USA.</p>
+                              <a href="" style="float: right; font-size: smaller;">view profile...</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6">
+                        <div class="media">
+                            <img class="d-flex mr-3 rounded-circle" src="images/new/team/team-1.jpg" alt="">
+                            <div class="media-body">
+                              <h5 class="mt-0">Eng. Modest MUGABO</h5>
+                              <p style="font-size: smaller;">Air Quality Specialist</p>
+                              <p style="text-align: justify;">Ten years of experience in air quality monitoring, data analysis, and processing. Field Supervisor.</p>
+                              <a href="" style="float: right; font-size: smaller;">view profile...</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row mt-3">
+                    <div class="col-lg-6">
+                        <div class="media">
+                            <img class="d-flex mr-3 rounded-circle" src="images/new/team/team-2.jpg" alt="">
+                            <div class="media-body">
+                              <h5 class="mt-0">Eng. Elyse DUSHIMIRIMANA</h5>
+                              <p style="font-size: smaller;">Chief Finance and Markerting Officer</p>
+                              <p style="text-align: justify;">Master’s of Science in Electrical power systems from UR-African Center of Excellence for energy sustainable development, Bachelor's degree in Electrical Engineering.</p>
+                              <a href="" style="float: right; font-size: smaller;">view profile...</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6">
+                        <div class="media">
+                            <img class="d-flex mr-3 rounded-circle" src="images/new/team/team-3.jpg" alt="">
+                            <div class="media-body">
+                              <h5 class="mt-0">Eng. Felix NIYOMUGABO</h5>
+                              <p style="font-size: smaller;">Site Engineer</p>
+                              <p style="text-align: justify;">Professional technologist (IER) Construction technology, 5 years of experience in civil engineering works.</p>
+                              <a href="" style="float: right; font-size: smaller;">view profile...</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php include('layouts/footer.php') ?>       
+        <?php include('layouts/scripts.php') ?>       
+    </body>
+</html>
