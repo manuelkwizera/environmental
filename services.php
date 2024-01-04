bcff8eed2fd88f93dab080adb16eea64c0e25c1a
manuelkwizera <kwizeraemmanuel11@gmail.com>
2024-01-04 10:30:35 +0530

    changing file extention

--- services.php
+++ services.php
@@ -7,7 +7,7 @@
             <div class="breadcrumb-main">
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item">
-                        <a href="index.html">Home</a>
+                        <a href="index.php">Home</a>
                     </li>
                     <li class="breadcrumb-item active">Services</li>
                 </ol>
@@ -21,7 +21,7 @@
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col-lg-6">
-                                        <a href="services/atmospheric-air-quality-measurements.html">
+                                        <a href="services/atmospheric-air-quality-measurements.php">
                                             <img class="img-fluid rounded" src="images/new/services/service-22.jpg" alt="" />
                                         </a>
                                     </div>
@@ -29,7 +29,7 @@
                                     <div class="col-lg-6">
                                         <h2 class="card-title">Ambient and indoor air quality measurements</h2>
                                         <p class="card-text">Air quality monitoring, including continuous data collection and analysis of pollutants (CO, O3, SO2, PM2.5, PM10, NOx) and industrial emissions monitoring with real-time data for compliance guidance.</p>
-                                        <a href="services/ambient-and-indoor-air-quality-measurements.html" class="btn btn-primary">Read More &rarr;</a>
+                                        <a href="services/ambient-and-indoor-air-quality-measurements.php" class="btn btn-primary">Read More &rarr;</a>
                                     </div>
                                 </div>
                             </div>
@@ -39,7 +39,7 @@
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col-lg-6">
-                                        <a href="services/chimney-and-stack-emissions-monitoring.html">
+                                        <a href="services/chimney-and-stack-emissions-monitoring.php">
                                             <img class="img-fluid rounded" src="images/new/services/list/service-4.jpg" alt="" />
                                         </a>
                                     </div>
@@ -47,7 +47,7 @@
                                     <div class="col-lg-6">
                                         <h2 class="card-title">Chimney and Stack Emissions Monitoring</h2>
                                         <p class="card-text">Our relentless commitment to cutting-edge Research and Development fuels pioneering innovation and sustainable progress. We thrive on pushing technological boundaries and crafting ingenious solutions for complex challenges.</p>
-                                        <a href="services/chimney-and-stack-emissions-monitoring.html" class="btn btn-primary">Read More &rarr;</a>
+                                        <a href="services/chimney-and-stack-emissions-monitoring.php" class="btn btn-primary">Read More &rarr;</a>
                                     </div>
                                 </div>
                             </div>
@@ -57,7 +57,7 @@
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col-lg-6">
-                                        <a href="services/noise-level-monitoring-for-public-events.html">
+                                        <a href="services/noise-level-monitoring-for-public-events.php">
                                             <img class="img-fluid rounded" src="images/new/services/list/service-15.jpeg" alt="" />
                                         </a>
                                     </div>
@@ -65,7 +65,7 @@
                                     <div class="col-lg-6">
                                         <h2 class="card-title">Noise Pollution  Monitoring  And Advisory</h2>
                                         <p class="card-text">We excel in technical testing, securing product and system reliability. With expertise, cutting-edge facilities, and tailored services, we offer functional, performance, and security testing, ensuring compliance and precision in your technical assets.</p>
-                                        <a href="services/noise-pollution-monitoring-and-advisory.html" class="btn btn-primary">Read More &rarr;</a>
+                                        <a href="services/noise-pollution-monitoring-and-advisory.php" class="btn btn-primary">Read More &rarr;</a>
                                     </div>
                                 </div>
                             </div>
@@ -75,14 +75,14 @@
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col-lg-6">
-                                        <a href="services/vibration-monitoring-and-advisory-for-blasting-activities.html">
+                                        <a href="services/vibration-monitoring-and-advisory-for-blasting-activities.php">
                                             <img class="img-fluid rounded" src="images/new/services/list/service-2.jpg" alt="" />
                                         </a>
                                     </div>
                                     <div class="col-lg-6">
                                         <h2 class="card-title">Vibration Monitoring and Advisory for Blasting Activities</h2>
                                         <p class="card-text">We excel in technical testing, securing product and system reliability. With expertise, cutting-edge facilities, and tailored services, we offer functional, performance, and security testing, ensuring compliance and precision in your technical assets.</p>
-                                        <a href="services/vibration-monitoring-and-advisory-for-blasting-activities.html" class="btn btn-primary">Read More &rarr;</a>
+                                        <a href="services/vibration-monitoring-and-advisory-for-blasting-activities.php" class="btn btn-primary">Read More &rarr;</a>
                                     </div>
                                 </div>
                             </div>
@@ -92,14 +92,14 @@
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col-lg-6">
-                                        <a href="services/wastewater-management-and-treatment.html">
+                                        <a href="services/wastewater-management-and-treatment.php">
                                             <img class="img-fluid rounded" src="images/new/services/list/service-5.jpeg" alt="" />
                                         </a>
                                     </div>
                                     <div class="col-lg-6">
                                         <h2 class="card-title">Wastewater Management and Treatment</h2>
                                         <p class="card-text">We excel in technical testing, securing product and system reliability. With expertise, cutting-edge facilities, and tailored services, we offer functional, performance, and security testing, ensuring compliance and precision in your technical assets.</p>
-                                        <a href="services/wastewater-management-and-treatment.html" class="btn btn-primary">Read More &rarr;</a>
+                                        <a href="services/wastewater-management-and-treatment.php" class="btn btn-primary">Read More &rarr;</a>
                                     </div>
                                 </div>
                             </div>
@@ -109,14 +109,14 @@
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col-lg-6">
-                                        <a href="services/environmental-health-and-safety-trainings-and-advisory.html">
+                                        <a href="services/environmental-health-and-safety-trainings-and-advisory.php">
                                             <img class="img-fluid rounded" src="images/new/services/list/services-img-09.png" alt="" />
                                         </a>
                                     </div>
                                     <div class="col-lg-6">
                                         <h2 class="card-title">Environmental, health and Safety Trainings and Advisory</h2>
                                         <p class="card-text">We excel in technical testing, securing product and system reliability. With expertise, cutting-edge facilities, and tailored services, we offer functional, performance, and security testing, ensuring compliance and precision in your technical assets.</p>
-                                        <a href="services/environmental-health-and-safety-trainings-and-advisory.html" class="btn btn-primary">Read More &rarr;</a>
+                                        <a href="services/environmental-health-and-safety-trainings-and-advisory.php" class="btn btn-primary">Read More &rarr;</a>
                                     </div>
                                 </div>
                             </div>
@@ -126,14 +126,14 @@
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col-lg-6">
-                                        <a href="services/environmental-and-safety-trainings-and-advisory.html">
+                                        <a href="services/environmental-and-safety-trainings-and-advisory.php">
                                             <img class="img-fluid rounded" src="images/new/services/list/service-8.jpeg" alt="" />
                                         </a>
                                     </div>
                                     <div class="col-lg-6">
                                         <h2 class="card-title">Environmental and social Impact assessment.</h2>
                                         <p class="card-text">We excel in technical testing, securing product and system reliability. With expertise, cutting-edge facilities, and tailored services, we offer functional, performance, and security testing, ensuring compliance and precision in your technical assets.</p>
-                                        <a href="services/environmental_and_social_impact_assessment.html" class="btn btn-primary">Read More &rarr;</a>
+                                        <a href="services/environmental_and_social_impact_assessment.php" class="btn btn-primary">Read More &rarr;</a>
                                     </div>
                                 </div>
                             </div>
